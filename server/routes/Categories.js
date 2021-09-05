f2037d78607ea5ecdb7b8c494177999354bd8d65
marioattieh <mario.attieh.2@gmail.com>
2021-09-05 17:33:53 +0300

    Server running

--- server/routes/Categories.js
+++ server/routes/Categories.js
@@ -2,21 +2,44 @@ const express = require("express");
 const router = express.Router();
 const Cat = require("../models/Categories");
 
-router.get("/", (req, res) => {});
+router.get("/", async (req, res) => {
+  try {
+    const cats = await Cat.find();
+    res.json(cats);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const cat = new Cat({
     title: req.body.title,
     body: req.body.body,
   });
-  cat
-    .save()
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  try {
+    const savedCat = await cat.save();
+    res.json(savedCat);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.get("/:catId", async (req, res) => {
+  try {
+    const cat = await Cat.findById(req.params.catId);
+    res.json(cat);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.delete("/:catId", async (req, res) => {
+  try {
+    const deletedCat = await Cat.deleteOne({ _id: req.params.catId });
+    res.json(deletedCat);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 module.exports = router;
