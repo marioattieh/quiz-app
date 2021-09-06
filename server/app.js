e3cab5d794a3abccf323518f171592436bd0a109
marioattieh <mario.attieh.2@gmail.com>
2021-09-06 23:49:44 +0300

    Final env

--- server/app.js
+++ server/app.js
@@ -1,25 +1,19 @@
+require("dotenv/config");
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv/config");
 const bodyParser = require("body-parser");
-const app = express();
 const cors = require("cors");
-
 const catRoute = require("./routes/Categories");
 const quizRoute = require("./routes/Quizzes");
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-
 app.use("/Categories", catRoute);
 app.use("/Quizzes", quizRoute);
 
-app.get("/", (req, res) => {
-  res.send("Home");
-});
-
 mongoose.connect(process.env.DB_CONN, { useNewUrlParser: true }, () => {
-  console.log("Hola");
+  console.log("DB connected");
 });
 
 app.listen(5000);
